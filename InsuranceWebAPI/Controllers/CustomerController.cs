58f646145bcc14c1f78f3d4e20160daa2793f86d
phbraganca <31511829+phbraganca@users.noreply.github.com>
2020-01-13 11:48:58 -0300

    v1.0

---
+++ InsuranceWebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Insurance.Domain.Interfaces;
+using Insurance.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceWebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomer _customer;
+        private Customer _obj = new Customer();
+
+        // GET: api/Customer
+
+        public CustomerController(ICustomer customer)
+        {
+            _customer = customer;
+
+        }
+        
+        [HttpGet]
+        [ActionName("GetAllCustomer")]
+        public IEnumerable<Customer> GetAll()
+        {                        
+            return _customer.GetAll(_obj).ToList();            
+        }
+        
+        // GET: api/Customer/5
+        [HttpGet("{id}", Name = "Get")]
+        [ActionName("GetCustomer")]
+        public Customer Get(int id)
+        {
+            _obj.CustomerId = id;
+            var customer = _customer.Get(_obj);            
+            return customer;
+        }
+        
+        // POST: api/Customer
+        [HttpPost]
+        [ActionName("InsertCustomer")]
+        public void Post(Customer obj)
+        {
+            if (_customer.CheckTaxIdentification(obj.TaxIdentification))
+            {
+                _customer.Save(obj);
+            }
+            else
+            {
+                throw new ArgumentException($"CPF {obj.TaxIdentification} Inválido.");
+            }
+            
+        }
+
+        // PUT: api/Customer/5
+        [HttpPut("{id}")]
+        [ActionName("UpdateCustomer")]
+        public void Put(int id, Customer obj)
+        {
+            _obj = obj;
+            var updateCustomer = _customer.Get(_obj);
+
+            updateCustomer.Address = obj.Address;
+            updateCustomer.Age = obj.Age;
+            updateCustomer.DateOfBith = obj.DateOfBith;
+            updateCustomer.Name = obj.Name;
+            updateCustomer.TaxIdentification = obj.TaxIdentification;
+
+            _customer.Update(updateCustomer);
+
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        [ActionName("DeleteCustomer")]
+        public void Delete(int id)
+        {
+            _obj.CustomerId = id;
+            _customer.Delete(_obj);
+
+        }
+    }
+}
---
+++ InsuranceWebAPI/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Insurance.Domain;
+using Insurance.Domain.Business;
+using Insurance.Domain.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SimpleInjector;
+using SimpleInjector.Integration.Web.Mvc;
+using SimpleInjector.Lifestyles;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace InsuranceWebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddScoped<ICustomer, CustomerBO>();
+            services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+            services.AddScoped(typeof(DataContext<>));
+            services.AddScoped<IConnection, Connection>();
+            
+
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Insurance", Version = "v1" });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            
+            app.UseSwagger();
+          
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Insurance v1");
+            });
+
+            app.UseHttpsRedirection();
+            app.UseMvc();              
+
+        }
+    }
+}
